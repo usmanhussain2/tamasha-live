615793d6fa9cf849718e90487ab33ba510b93467
usmanhussain2 <usmanam990@gmail.com>
2026-02-14 17:49:09 +0500

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,16 @@
+const video = document.getElementById("video");
 
+if(Hls.isSupported()){
+
+const hls = new Hls({
+
+liveSyncDurationCount:1,
+liveMaxLatencyDurationCount:2
+
+});
+
+hls.loadSource(CONFIG.stream);
+
+hls.attachMedia(video);
+
+}
